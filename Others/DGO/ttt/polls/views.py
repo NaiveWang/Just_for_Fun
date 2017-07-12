7ada3e9f5f9f5199f2814633ebe7e30e8b8ef4dc
Unknown <wanhchiwen@yahoo.com>
2017-07-12 16:22:24 +0800

    I'm learning django!

--- Others/DGO/ttt/polls/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-def index(request):
-    return HttpResponse("Aloha Oe!")
-# Create your views here.
--- Others/djo/first/first/wsgi.py
+++ Others/djo/first/first/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for ttt project.
+WSGI config for first project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ttt.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "first.settings")
 
 application = get_wsgi_application()
