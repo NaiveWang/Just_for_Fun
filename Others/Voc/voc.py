770e23c9bdd4eac6caa0693418b9731b087003c6
naive wong <wanhchiwen@yahoo.com>
2017-07-26 21:42:45 +0800

    ...

--- Others/Voc/voc.py
+++ Others/Voc/voc.py
@@ -37,6 +37,26 @@ def SCAN():
 
     conn.commit()
     conn.close()
+def SCAN1():
+    conn = sqlite3.connect(database)
+    #cconn = sqlite3.connect('CET4.db3')
+    c = conn.cursor()
+    for row in c.execute("SELECT * FROM %s"%table+' where state <> -1'):
+        print('\n\n\n'+row[0].__str__()+'---'+row[1])
+        print('Press ENTER to get meaning.')
+        input()
+        print(row[2])
+        print('\tDid U get it? y to confirm/q to quit/others to mark.')
+        i=input()
+        if(i=='y'):
+            conn.execute("UPDATE %s SET STATE=-1 WHERE id="%table+row[0].__str__())
+        elif(i=='q'):
+            break
+        else:
+            conn.execute("UPDATE %s SET STATE=1 WHERE id="%table + row[0].__str__())
+
+    conn.commit()
+    conn.close()
 def EXPO():
     conn=sqlite3.connect(database)
     o=open('n_voc.txt', 'w')
@@ -48,4 +68,5 @@ def EXPO():
     o.close()
     conn.close()
 
+SCAN1()
 EXPO()
