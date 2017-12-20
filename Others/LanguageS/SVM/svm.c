bad164bcecddcdd5c2f6d86117e6f36f4a4735c3
naive wong <wanhchiwen@yahoo.com>
2017-12-20 15:47:01 +0800

    done it

--- Others/LanguageS/SVM/svm.c
+++ Others/LanguageS/SVM/svm.c
@@ -23,7 +23,7 @@ typedef struct DataList
   void *data;
 }delem;
 long reg;
-int insdata[4];
+int *insdata;
 
 char flag;//ZERO/SIGN/N/N/N/N/RRD/WRD
 int pc,sp;
@@ -48,6 +48,11 @@ void changeFlagI(int *target)
   if(*target < 0) flag |= 0x02;
 }
 void changeFlagL(long *target)
+{
+  if(*target == 0) flag |= 0x01;
+  if(*target < 0) flag |= 0x02;
+}
+void changeFlagR(double *target)
 {
   if(*target == 0) flag |= 0x01;
   if(*target < 0) flag |= 0x02;
@@ -78,6 +83,7 @@ void *MOVC()
     *insdata=-1;
     *HALT();
   }
+  pc++;
 }
 void *MOVI()
 {//source/destination
@@ -92,6 +98,7 @@ void *MOVI()
     *insdata=-1;
     *HALT();
   }
+  pc++;
 }
 void *MOVL()
 {//source/destination
@@ -106,6 +113,7 @@ void *MOVL()
     *insdata=-1;
     *HALT();
   }
+  pc++;
 }
 void *JUMP()
 {
@@ -119,226 +127,22 @@ void *JMPN()
 {
   if(*(insdata+1)^flag) pc+=*insdata;
 }
-void *ADDC()
-{//source1/source2/destination
-  if(segd[*insdata].size == segd[*(insdata+2)].size == segd[*(insdata+4)].size)
-  {
-    char *temp0 = addrFetcher(insdata+2,insdata+3);
-    char *temp1 = addrFetcher(insdata,insdata+1);
-    char *temp2 = addrFetcher(insdata+4,insdata+5);
-    *temp2 = *temp0 + *temp1;
-    changeFlagC(temp2);
-  }
-  else
-  {
-    *insdata=-1;
-    *HALT();
-  }
-}
-void *ADDI()
-{//source1/source2/destination
-  if(segd[*insdata].size == segd[*(insdata+2)].size == segd[*(insdata+4)].size)
-  {
-    int *temp0 = addrFetcher(insdata+2,insdata+3);
-    int *temp1 = addrFetcher(insdata,insdata+1);
-    int *temp2 = addrFetcher(insdata+4,insdata+5);
-    *temp2 = *temp0 + *temp1;
-    changeFlagL(temp2);
-  }
-  else
-  {
-    *insdata=-1;
-    *HALT();
-  }
-}
-void *ADDL()
-{//source1/source2/destination
-  if(segd[*insdata].size == segd[*(insdata+2)].size == segd[*(insdata+4)].size)
-  {
-    long *temp0 = addrFetcher(insdata+2,insdata+3);
-    long *temp1 = addrFetcher(insdata,insdata+1);
-    long *temp2 = addrFetcher(insdata+4,insdata+5);
-    *temp2 = *temp0 + *temp1;
-    changeFlagL(temp2);
-  }
-  else
-  {
-    *insdata=-1;
-    *HALT();
-  }
-}
-void *SUBC()
-{//source1/source2/destination
-    if(segd[*insdata].size == segd[*(insdata+2)].size == segd[*(insdata+4)].size)
-    {
-      char *temp0 = addrFetcher(insdata+2,insdata+3);
-      char *temp1 = addrFetcher(insdata,insdata+1);
-      char *temp2 = addrFetcher(insdata+4,insdata+5);
-      *temp2 = *temp0 - *temp1;
-      changeFlagC(temp2);
-    }
-    else
-    {
-      *insdata=-1;
-      *HALT();
-    }
-}
-void *SUBI()
-{//source1/source2/destination
-    if(segd[*insdata].size == segd[*(insdata+2)].size == segd[*(insdata+4)].size)
-    {
-      int *temp0 = addrFetcher(insdata+2,insdata+3);
-      int *temp1 = addrFetcher(insdata,insdata+1);
-      int *temp2 = addrFetcher(insdata+4,insdata+5);
-      *temp2 = *temp0 - *temp1;
-      changeFlagI(temp2);
-    }
-    else
-    {
-      *insdata=-1;
-      *HALT();
-    }
-}
-void *SUBL()
-{//source1/source2/destination
-    if(segd[*insdata].size == segd[*(insdata+2)].size == segd[*(insdata+4)].size)
-    {
-      long *temp0 = addrFetcher(insdata+2,insdata+3);
-      long *temp1 = addrFetcher(insdata,insdata+1);
-      long *temp2 = addrFetcher(insdata+4,insdata+5);
-      *temp2 = *temp0 - *temp1;
-      changeFlagL(temp2);
-    }
-    else
-    {
-      *insdata=-1;
-      *HALT();
-    }
-}
-void *MULC()
-{
-  //source1/source2/destination
-      if(segd[*insdata].size == segd[*(insdata+2)].size == segd[*(insdata+4)].size)
-      {
-        char *temp0 = addrFetcher(insdata+2,insdata+3);
-        char *temp1 = addrFetcher(insdata,insdata+1);
-        char *temp2 = addrFetcher(insdata+4,insdata+5);
-        *temp2 = *temp0 * *temp1;
-        changeFlagC(temp2);
-      }
-      else
-      {
-        *insdata=-1;
-        *HALT();
-      }
-}
-void *MULI()
-{//source1/source2/destination
-    if(segd[*insdata].size == segd[*(insdata+2)].size == segd[*(insdata+4)].size)
-    {
-      int *temp0 = addrFetcher(insdata+2,insdata+3);
-      int *temp1 = addrFetcher(insdata,insdata+1);
-      int *temp2 = addrFetcher(insdata+4,insdata+5);
-      *temp2 = *temp0 * *temp1;
-      changeFlagI(temp2);
-    }
-    else
-    {
-      *insdata=-1;
-      *HALT();
-    }
-}
-void *MULL()
-{//source1/source2/destination
-    if(segd[*insdata].size == segd[*(insdata+2)].size == segd[*(insdata+4)].size)
-    {
-      long *temp0 = addrFetcher(insdata+2,insdata+3);
-      long *temp1 = addrFetcher(insdata,insdata+1);
-      long *temp2 = addrFetcher(insdata+4,insdata+5);
-      *temp2 = *temp0 * *temp1;
-      changeFlagL(temp2);
-    }
-    else
-    {
-      *insdata=-1;
-      *HALT();
-    }
-}
-void *DIVC()
-{
-  
-}
-void *DIVI()
-{
-}
-void *DIVL()
-{
-}
-void *_CMP()
-{
-}
-void *NEGC()
-{
-}
-void *NEGI()
-{
-}
-void *NEGL()
-{
-}
-void *NOTC()
-{
-}
-void *NOTI()
-{
-}
-void *NOTL()
-{
-}
-void *EORC()
-{
-}
-void *EORI()
-{
-}
-void *EORL()
-{
-}
-void *ANDC()
-{
-}
-void *ANDI()
-{
-}
-void *ANDL()
-{
-}
-void *_ORC()
-{
-}
-void *_ORI()
-{
-}
-void *_ORL()
-{
-}
-void *TSTC()
-{
-}
-void *TSTI()
-{
-}
-void *TSTL()
-{
-}
-void *RAND()
-{
-}
 void *CALL()
 {
+  segd[sizeofsegd+sp].data=malloc(5);
+  char *temp0=segd[sizeofsegd+sp].data;
+  int *temp1=segd[sizeofsegd+sp].data + 1;
+  *temp0=flag;
+  *temp1=pc;
+  sp++;
+  pc=segi[pc].data[0];
 }
 void *RETN()
 {
+  sp--;
+  flag=(char)segd[sizeofsegd+sp].data;
+  pc=(int)(segd[sizeof+sp].data + 1) + 1;
+  free(segd[sizeofsegd+sp].data);
 }
 void *PUTC()
 {
@@ -352,20 +156,180 @@ void *PUTI()
 void *GETI()
 {
 }
-idt ins[INSTRUCTION]={  {"MOVC",4,MOVC},{"MOVI",4,MOVI},{"MOVL",4,MOVL},{"JUMP",1,JUMP},{"JMPF",2,JMPF},{"JMPN",2,JMPN},{"HALT",1,HALT},
-                        {"ADDC",3,ADDC},{"ADDI",3,ADDI},{"ADDL",3,ADDL},{"SUBC",3,SUBC},{"SUBI",3,SUBI},{"SUBL",3,SUBL},{"MULC",3,MULC},
-                        {"MULI",3,MULI},{"MULL",3,MULL},{"DIVC",3,DIVC},{"DIVI",3,DIVI},{"DIVL",3,DIVL},{"_CMP",2,_CMP},{"NEGC",2,NEGC},
-                        {"NEGI",2,NEGI},{"NEGL",2,NEGL},{"NOTC",2,NOTC},{"NOTI",2,NOTI},{"NOTL",2,NOTL},{"EORC",3,EORC},{"EORI",3,EORI},
-                        {"EORL",3,EORL},{"ANDC",3,ANDC},{"ANDI",3,ANDI},{"ANDL",3,ANDL},{"_ORC",3,_ORC},{"_ORI",3,_ORI},{"_ORL",3,_ORL},
-                        {"TSTC",1,TSTC},{"TSTI",1,TSTI},{"TSTL",1,TSTL},{"RAND",1,RAND},{"CALL",3,CALL},{"RETN",3,RETN},{"PUTC",1,PUTC},
-                        {"PUTI",1,PUTI},{"GETC",1,GETC},{"GETI",1,GETI}};
-
-int decoder(char* s)
+//value/destination//
+void *ALOC()
+{
+  char *s = insdata;
+  char *t = addrFetcher(insdata+1,insdata+2);
+  *t = *s;
+  pc++;
+}
+void *ALOI()
+{
+  int *s = insdata;
+  int *t = addrFetcher(insdata+1,insdata+2);
+  *t = *s;
+  pc++;
+}
+void *ALOL()
+{
+  long *s = insdata;
+  long *t = addrFetcher(insdata+2,insdata+3);
+  *t = *s;
+  pc++;
+}
+void *ALOR()
+{
+  double *s = insdata;
+  double *t = addrFetcher(insdata+2,insdata+3);
+  *t = *s;
+  pc++;
+}
+//type,Source1,2/destination/
+void *ARBC()
+{
+  char *s1=addrFetcher(insdata+1,insdata+2);
+  char *s2=addrFetcher(insdata+3,insdata+4);
+  char *d=addrFetcher(insdata+5,insdata+6);
+  switch(insdata[0])
+  {
+    case 0x0:*d = *s1 + *s2;break;
+    case 0x1:*d = *s1 - *s2;break;
+    case 0x2:*d = *s1 * *s2;break;
+    case 0x3:*d = *s1 / *s2;break;
+    case 0x4:*d = *s1 % *s2;break;
+    case 0x5:*d = *s1 << *s2;break;
+    case 0x6:*d = *s1 >> *s2;break;
+    case 0x7:*d = *s1 & *s2;break;
+    case 0x8:*d = *s1 | *s2;break;
+    case 0x9:*d = *s1 ^ *s2;break;
+  }
+  changeFlagC(d);
+  pc++;
+}
+void *ARBI()
+{
+  int *s1=addrFetcher(insdata+1,insdata+2);
+  int *s2=addrFetcher(insdata+3,insdata+4);
+  int *d=addrFetcher(insdata+5,insdata+6);
+  switch(insdata[0])
+  {
+    case 0x0:*d = *s1 + *s2;break;
+    case 0x1:*d = *s1 - *s2;break;
+    case 0x2:*d = *s1 * *s2;break;
+    case 0x3:*d = *s1 / *s2;break;
+    case 0x4:*d = *s1 % *s2;break;
+    case 0x5:*d = *s1 << *s2;break;
+    case 0x6:*d = *s1 >> *s2;break;
+    case 0x7:*d = *s1 & *s2;break;
+    case 0x8:*d = *s1 | *s2;break;
+    case 0x9:*d = *s1 ^ *s2;break;
+  }
+  changeFlagI(d);
+  pc++;
+}
+void *ARBL()
+{
+  long *s1=addrFetcher(insdata+1,insdata+2);
+  long *s2=addrFetcher(insdata+3,insdata+4);
+  long *d=addrFetcher(insdata+5,insdata+6);
+  switch(insdata[0])
+  {
+    case 0x0:*d = *s1 + *s2;break;
+    case 0x1:*d = *s1 - *s2;break;
+    case 0x2:*d = *s1 * *s2;break;
+    case 0x3:*d = *s1 / *s2;break;
+    case 0x4:*d = *s1 % *s2;break;
+    case 0x5:*d = *s1 << *s2;break;
+    case 0x6:*d = *s1 >> *s2;break;
+    case 0x7:*d = *s1 & *s2;break;
+    case 0x8:*d = *s1 | *s2;break;
+    case 0x9:*d = *s1 ^ *s2;break;
+  }
+  changeFlagL(d);
+  pc++;
+}
+void *ARBR()
+{
+  double *s1=addrFetcher(insdata+1,insdata+2);
+  double *s2=addrFetcher(insdata+3,insdata+4);
+  double *d=addrFetcher(insdata+5,insdata+6);
+  switch(insdata[0])
+  {
+    case 0x0:*d = *s1 + *s2;break;
+    case 0x1:*d = *s1 - *s2;break;
+    case 0x2:*d = *s1 * *s2;break;
+    case 0x3:*d = *s1 / *s2;break;
+  }
+  changeFlagR(d);
+  pc++;
+}
+void *ARUC()
+{
+  char *s=addrFetcher(insdata+1,insdata+2);
+  char *d=addrFetcher(insdata+3,insdata+4);
+  switch(insdata[0])
+  {
+    case 0x0:*d = ~ *s; break;
+    case 0x1:*d = - *s; break;
+  }
+  changeFlagC(d);
+  pc++;
+}
+void *ARUI()
+{
+  int *s=addrFetcher(insdata+1,insdata+2);
+  int *d=addrFetcher(insdata+3,insdata+4);
+  switch(insdata[0])
+  {
+    case 0x0:*d = ~ *s; break;
+    case 0x1:*d = - *s; break;
+  }
+  changeFlagI(d);
+  pc++;
+}
+void *ARUL()
+{
+  long *s=addrFetcher(insdata+1,insdata+2);
+  long *d=addrFetcher(insdata+3,insdata+4);
+  switch(insdata[0])
+  {
+    case 0x0:*d = ~ *s; break;
+    case 0x1:*d = - *s; break;
+  }
+  changeFlagL(d);
+  pc++;
+}
+void *NEGR()
+{
+  double *s=addrFetcher(insdata+1,insdata+2);
+  double *d=addrFetcher(insdata+3,insdata+4);
+  /**switch(insdata[0])
+  {
+    case 0x0:*d = ~ *s; break;
+    //case 0x1:*d = - *s; break;
+  }*/
+  *d = - *s;
+  changeFlagR(d);
+  pc++;
+}
+idt ins[INSTRUCTION]={  {"MOVC",4,MOVC},{"MOVI",4,MOVI},{"MOVL",4,MOVL},
+                        {"JUMP",1,JUMP},{"JMPF",2,JMPF},{"JMPN",2,JMPN},
+                        {"HALT",1,HALT},
+                        {"CALL",3,CALL},{"RETN",3,RETN},
+                        {"PUTC",1,PUTC},{"PUTI",1,PUTI},{"GETC",1,GETC},{"GETI",1,GETI},
+                        {"ALOC",2,ALOC},{"ALOI",2,ALOI},{"ALOL",3,ALOL},{"ALOR",3,ALOR},//allocation
+                        {"ARBC",7,ARBC},{"ARBI",7,ARBI},{"ARBL",7,ARBL},{"ARBR",7,ARBR},//arithmatic binary
+                        {"ARUC",2,ARUC},{"ARUI",2,ARUI},{"ARUL",2,ARUL},{"NEGR",2,NEGR}//arithmatic unary
+                        };
+int decoder(int* s)
 {
   int i=INSTRUCTION;
+  int *p;
   while(i--)
   {
-    if(ins[i].instruction[0]==s[0] && ins[i].instruction[1]==s[1] && ins[i].instruction[2]==s[2] && ins[i].instruction[3]==s[3])
+    p=ins[i].instruction;
+    if(*s == *p)
       return i;
   }
 }
@@ -409,12 +373,22 @@ void readBin(char *s)
 }
 void RunABlock()//run a block of code without branch
 {
-  ;
+  static void *(*instruction)()=NULL;
+  while(instruction != HALT || JMPF || JMPN)
+  {
+    instruction=segi[pc].instruction;
+    insdata=segi[pc].data;
+    *instruction;
+  }
 }
 void VMstart()
 {
   //reset pointer.
   pc=sp=0;
+  while(segi[pc].instruction != HALT)
+  {
+    RunABlock();
+  }
 }
 
 int main(int argc,char **argv)
