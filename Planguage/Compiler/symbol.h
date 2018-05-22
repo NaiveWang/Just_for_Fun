3a2dab4d3317ebd47e1ff0c4b617c2eb1b358d2f
Elvin Wong <wanhchiwen@yahoo.com>
2018-05-22 23:36:49 +0800

    ..

--- Planguage/Compiler/symbol.h
+++ Planguage/Compiler/symbol.h
@@ -1,8 +1,25 @@
 #ifndef SYMBOL_H
 #define SYMBOL_H
 /**
-  * this file contains
+  * this file contains the fucking
   *
   *
   **/
+/* type define & macro */
+#define LENGTH_ID 32
+#define TYP_CHAR 'C'
+#define TYP_INT 'I'
+#define TYP_REAL 'R'
+#define TYP_STRING 'S'
+typedef struct identifierList
+{
+    char type;
+    char name[LENGTH_ID];
+}id
+typedef struct constantList
+{
+    char type;
+    void *content;
+}constant;
+/* function section */
 #endif
