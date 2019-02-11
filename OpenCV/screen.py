16dad2a14e34ba9f72e0a4e3eb9becb29f2b312b
ELvin Wang <wanhchiwen@yahoo.com>
2019-02-11 21:10:44 +0800

    make screenshot and recognize

--- OpenCV/screen.py
+++ OpenCV/screen.py
@@ -2,8 +2,10 @@ import pyscreenshot as ImageGrab
 import time
 import numpy as np
 import cv2
-from PyQt5.QtWidgets import QApplication
-import sys
+
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import GObject, GLib
 
 
 #import matplotlab.pyplot as plt
@@ -16,31 +18,36 @@ import sys
 #import wx
 #wx.App()
 
-face_csc = cv2.CascadeClassifier('haarcascade_upperbody.xml')
+face_csc = cv2.cuda.CascadeClassifier_create('haarcascade_upperbody.xml')
+GObject.threads_init()
 
-app = QApplication(sys.argv)
-screen = QApplication.primaryScreen()
 while True:
 
 
-    raw = screen.grabWindow(0, 0, 0, 100, 100).toImage()
+    #raw = screen.grabWindow(0, 0, 0, 100, 100).toImage()
     #print(type(raw))
-    #raw = ImageGrab.grab()
+    #print(raw)
+    raw = ImageGrab.grab(bbox=(0,0,1280,720))
+
+
     #print(type(raw))
-    print('@')
+    print(raw)
+    #print('@')
+    #print(raw)
     img = np.array(raw)
-    print('@@')
+    #print('@@')
     frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    print('@@@')
+    #print('@@@')
     faces = face_csc.detectMultiScale(frame, 1.1, 4)
-    print('@@@')
+    #print('@@@')
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,0), 5)
-    print('@@@@')
-    cv2.imshow("frame", frame)
-    print('@@@@@')
-    cv2.waitKey(50)
-    print('@@@@@@')
+        #cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,0), 5)
+        print('head')
+    #print('@@@@')
+    # GLib.idle_add(lambda:cv2.imshow("frame", frame))
+    #print('@@@@@')
+    cv2.waitKey(5)
+    #print('@@@@@@')
 
 
     #time.sleep(1)
