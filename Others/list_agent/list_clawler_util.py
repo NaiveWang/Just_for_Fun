b583e1c0544a324b4cc425fb4aa8ea030576fae5
Elvin Wang <wanhchiwen@yahoo.com>
2019-05-15 17:48:39 +0800

    project finished
    document may needed.

--- Others/list_agent/list_clawler_util.py
+++ Others/list_agent/list_clawler_util.py
@@ -1,15 +1,20 @@
  
 from urllib import request as req
 from lxml import html
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
-# bw = webdriver.Chrome()
+import time
+import _init_
+
+
 
 url1="http://www.qhdkjj.gov.cn/news-list-zdhd.html"
 xph1="/html/body/table/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[4]/table/tbody/tr/td/table/tbody/tr[3]/td/table/tbody/tr/td/div/div/div/form/table/tbody/tr[1]/td/table/tbody/tr/td/table/tbody/tr/td[1]/a"
 xph1n="/html/body/table/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[4]/table/tbody/tr/td/table/tbody/tr[3]/td/table/tbody/tr/td/div/div/div/form/table/tbody/tr[2]/td/div/ul/li[2]/a[3]"
 url1u='http://www.qhdkjj.gov.cn/news-list-zdhd-<<<>>>1<<<>>>.html'
+
+url2="http://www.gzsi.gov.cn/gzsi/kpzl_kpjd/list.shtml"
+xph2="/html/body/div[1]/div[3]/div[2]/div[2]/ul/li/a"
+xph2b='//*[@id="page_div"]/span[8]/a'
 
 def jsfree_single(url, xpath):
     tree = html.parse(req.urlopen(url))
@@ -50,7 +55,33 @@ def jsfree_multi_url(url, xpath):
             url[1]+=1
     except Exception as e:
         return dic
+def js_single(url, xpath, delay):
+    dic={}
+    _init_.bw.get(url)
+    time.sleep(delay)
+    elm=_init_.bw.find_elements_by_xpath(xpath)
+    for e in elm:
+        dic[e.text]=e.get_attribute("href")
+        #print(e.text, e.get_attribute("href"))
+    return dic
+
+def js_multi_click(url, xpath, xpath_next, delay, page=5):
+    dic={}
+    try:
+        _init_.bw.get(url)
+        for i in range(0,page):
+            time.sleep(delay)
+            elm=_init_.bw.find_elements_by_xpath(xpath)
+            for e in elm:
+                dic[e.text]=e.get_attribute("href")
+                #print(e.text, e.get_attribute("href"))
+            btn = _init_.bw.find_element_by_xpath(xpath_next)
+            btn.click()
+    except Exception as e:
+        print(e)
+        return dic
+    return dic
 
 
 
-#jsfree_multi_url(url1u,xph1)
+# print(js_multi_click(url2,xph2,xph2b))
--- Others/list_agent/server.py
+++ Others/list_agent/server.py
@@ -3,13 +3,30 @@
 from flask import Flask, render_template, request
 import list_clawler_util as cwr
 app = Flask(__name__)
+@app.route('/')
+def top():
+    return render_template('top.html', link="/")
+
+@app.route("/", methods=['POST'])
+def gen():
+    api=request.form['api']
+    url=request.form['url'].replace('&','%26')
+    xpath=request.form['xph']
+    xpathn=request.form['xphn']
+    delay=request.form['delay']
+    lgen=api+"?url="+url+"&xpath="+xpath+"&xpathn="+xpathn+"&delay="+delay
+    
+    return render_template('top.html', link=lgen)
+
+
+
 @app.route('/jsfree')
 def jsfree():
     url = request.args.get('url')
     xpath = request.args.get('xpath')
     list = cwr.jsfree_single(url,xpath)
     #print(list)
-    return render_template('top.html', list = list)
+    return render_template('list.html', list = list)
 @app.route('/jsfree/xpath')
 def jsfree_xpath():
     url = request.args.get('url')
@@ -17,15 +34,38 @@ def jsfree_xpath():
     xpathn = request.args.get('xpathn')
     list = cwr.jsfree_multi_xpath(url,xpath,xpathn)
     #print(list)
-    return render_template('top.html', list = list)
+    return render_template('list.html', list = list)
 @app.route('/jsfree/url')
 def jsfree_url():
     url = request.args.get('url')
     xpath = request.args.get('xpath')
     list = cwr.jsfree_multi_url(url,xpath)
     #print(list)
-    return render_template('top.html', list = list)
-
-
+    return render_template('list.html', list = list)
+@app.route('/js')
+def js():
+    url = request.args.get('url')
+    xpath = request.args.get('xpath')
+    try:
+        delay = int(request.args.get('delay'))
+    except Exception:
+        delay = 3
+    list = cwr.js_single(url,xpath,delay)
+    #print(list)
+    return render_template('list.html', list = list)
+@app.route('/js/click')
+def js_xpath():
+    url = request.args.get('url')
+    xpath = request.args.get('xpath')
+    xpathb = request.args.get('xpathn')
+    try:
+        delay = int(request.args.get('delay'))
+    except Exception:
+        delay = 3
+    #print(xpathb)
+    list = cwr.js_multi_click(url,xpath,xpathb,delay)
+    #print(list)
+    return render_template('list.html', list = list)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
+    
