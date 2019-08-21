755eee12d3d73795579ea0c882742febe4853d44
brainfart95 <wanhzhiwen@gmail.com>
2019-08-21 10:57:48 +0800

    add douban rating web server

--- Others/linux_gadgets/gpu_check_tensorflow.py
+++ Others/linux_gadgets/gpu_check_tensorflow.py
@@ -1,9 +1,11 @@
 from tensorflow.python.client import device_lib
-
+import tensorflow as tf
 print('check devices...')
 
 device_lib.list_local_devices()
 
 print('running a simple random gen...')
 
-print (tf.reduce_sum(tf.random_normal([10000, 10000])))
+
+
+print (tf.random.normal([10000, 10000]))
