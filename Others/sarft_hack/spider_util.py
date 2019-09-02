093da6e40c73285709cae8f4518056ecd7c22efe
NaiveWang <wanhzhiwen@gmail.com>
2019-09-02 17:55:17 +0800

    add new cutting edge spider

--- Others/sarft_hack/spider_util.py
+++ Others/sarft_hack/spider_util.py
@@ -60,8 +60,101 @@ def grab_list():
 
         #print(jsfree_single(e[1], xpath_l[0], "onclick"))
         #for k, v in ll.items():
-            c.execute("insert into list1(url)values(?)", (domain+el.split("',", 1)[0].replace('\n1ff8\n', ''), ))
+            c.execute("insert into list1(url)values(?)", (domain+el.split("',", 1)[0].replace('\r\n915\n\r', ''), ))
         c.execute("update list0 set tag=1, num=? where id=?", (len(l), e[0], ))
         db.commit()
     db.close()
-grab_list()
+def grab_blah():
+    db=sqlite3.connect(dbn)
+    c=db.cursor()
+    c.execute('select id, url from list1 where tag=0 order by random()')
+    r=c.fetchall()
+    for e in r:
+        print(e)
+        #print()
+        #for a in tree.xpath('/html/body/div/div/table/tbody/tr/td/table[3]/tbody/tr[2]/td'):
+        #    print('>>>', a.text)
+        #print(tree.xpath('/html/body/div/div/table/tbody/tr/td/table[3]/tbody/tr[2]/td').text)
+        #for a in tree.xpath('/html/body/div/div/table/tbody/tr/td/table[3]/tbody/tr[3]/td/span'):
+        #    print(a.text)
+        #print(tree.xpath('').text)
+        #print(tree.xpath('').text)
+        #print(tree.xpath('').text)
+        #print(tree.xpath('').text)
+        #print(tree.xpath('').text)
+        try:
+            c.execute('update list1 set tag=1, raw=? where id=?', (req.urlopen(e[1]).read().decode(), e[0]))
+            db.commit()
+        except Exception as e:
+            print(e)
+def debug0():
+    db=sqlite3.connect(dbn)
+    c=db.cursor()
+    c.execute('select id, url from list1 where tag=0')
+    r=c.fetchall()
+    for e in r:
+        print(e)
+        c.execute('update list1 set url=? where id=?', (e[1].replace('\r\n8f0\r\n', ''), e[0]))
+        db.commit()
+    c.close()
+    db.close()
+def truncate():
+    db=sqlite3.connect(dbn)
+    c=db.cursor()
+    c.execute('select id, raw from list1')
+    r=c.fetchall()
+    for e in r:
+        print(e)
+        c.execute('update list1 set raw=? where id=?', (e[1].split('<tr align="center">')[-1].split('</table>', 1)[0], e[0]))
+        
+    db.commit()
+    c.close()
+    db.close()
+def prune(keyword, nkeyword):
+    db=sqlite3.connect(dbn)
+    c=db.cursor()
+    c.execute('select id, raw from list1')
+    r=c.fetchall()
+    for e in r:
+        print(e)
+        c.execute('update list1 set raw=? where id=?', (e[1].replace(keyword, nkeyword), e[0]))
+        
+    db.commit()
+    c.close()
+    db.close()
+def prune_post(keyword, nkeyword):
+    db=sqlite3.connect(dbn)
+    c=db.cursor()
+    c.execute('select id, blah from blah')
+    r=c.fetchall()
+    for e in r:
+        print(e)
+        c.execute('update blah set blah=? where id=?', (e[1].replace(keyword, nkeyword), e[0]))
+        
+    db.commit()
+    c.close()
+    db.close()
+def extract():
+    db=sqlite3.connect(dbn)
+    c=db.cursor()
+    c.execute('select id, raw from list1')
+    r=c.fetchall()
+    for e in r:
+        ee = e[1].split('#$#')[1:]
+        col = [
+            e[0],
+            ee[0].split('</td>', 1)[0],
+            ee[1].split('</td>', 1)[0],
+            ee[2].split('</td>', 1)[0],
+            ee[3].split('</td>', 1)[0],
+            ee[4].split('</td>', 1)[0],
+            ee[5].split('</td>', 1)[0],
+            ee[7].split('</td>', 1)[0]]
+        c.execute('insert into blah(id, title, name, company, asshole, result, region, blah)values(?,?,?,?,?,?,?,?)', col)
+        #print(col)
+    db.commit()
+#debug0()
+#grab_blah()
+## grab_list()
+#prune('\n', '')
+prune_post('\u3000', '')
