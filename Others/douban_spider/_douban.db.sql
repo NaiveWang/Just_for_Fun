5036a3bb0aebcbba58bd52e70d81af15a21fce19
NaiveWang <wanhzhiwen@gmail.com>
2019-09-03 17:11:19 +0800

    export database structure

---
+++ Others/douban_spider/_douban.db.sql
@@ -0,0 +1,44 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "comments" (
+	"id"	INTEGER,
+	"pid"	INTEGER,
+	"uid"	INTEGER,
+	"text"	TEXT,
+	"create_time"	TEXT,
+	"parent_comment_id"	INTEGER,
+	PRIMARY KEY("id")
+);
+CREATE TABLE IF NOT EXISTS "post" (
+	"uri"	TEXT,
+	"id"	INTEGER,
+	"topic"	INTEGER,
+	"text"	TEXT,
+	"comments_count"	INTEGER,
+	"create_time"	TEXT,
+	"like_count"	INTEGER,
+	PRIMARY KEY("uri")
+);
+CREATE TABLE IF NOT EXISTS "user" (
+	"id"	INTEGER UNIQUE,
+	"uid"	TEXT,
+	"name"	TEXT,
+	"url_avatar"	TEXT,
+	"loc"	INTEGER,
+	PRIMARY KEY("id")
+);
+CREATE TABLE IF NOT EXISTS "topic" (
+	"id"	INTEGER,
+	"title"	INTEGER,
+	PRIMARY KEY("id")
+);
+CREATE TABLE IF NOT EXISTS "img" (
+	"pid"	INTEGER,
+	"url"	TEXT
+);
+CREATE TABLE IF NOT EXISTS "loc" (
+	"id"	INTEGER UNIQUE,
+	"name"	TEXT,
+	"uid"	TEXT,
+	PRIMARY KEY("id")
+);
+COMMIT;
