b9357a25c3b87ce511d5511f91d53577545386c1
NaiveWang <wanhzhiwen@gmail.com>
2019-09-04 14:03:36 +0800

    the real multi-threaded python spider

---
+++ Others/douban_spider/img_grabber.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+python img_grabber.py & P1=$!
+echo $P1
+python img_grabber.py & P2=$!
+echo $P2
+python img_grabber.py & P3=$!
+echo $P3
+python img_grabber.py
+#wait
+kill -9 $P1 $P2 $P3
