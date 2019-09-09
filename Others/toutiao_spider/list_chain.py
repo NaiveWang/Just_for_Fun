23c037f22ff8a79e18b2569e6c135979a0482baa
NaiveWang <wanhzhiwen@gmail.com>
2019-09-09 14:41:23 +0800

    add aggressive script

--- Others/toutiao_spider/list_chain.py
+++ Others/toutiao_spider/list_chain.py
@@ -13,42 +13,37 @@ headerz={
 "Cache-Control": "max-age=0",
 "Connection": "keep-alive",
 "Host": "www.toutiao.com",
+"Cookie": "tt_webid=6730777189916984835; WEATHER_CITY=%E5%8C%97%E4%BA%AC; tt_webid=6730777189916984835; csrftoken=1203577a586351ec44ccc51b100ed58c; s_v_web_id=3d26f78744e5c7d5f2136679b95cfb07; __tasessionId=d23el1ind1568007368379",
 "TE": "Trailers",
 "Upgrade-Insecure-Requests": "1",
 "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0"}
 db=sqlite3.connect('toutiao.db')
 c=db.cursor()
-c.execute('select seed from seeds')
+c.execute('select seed, category from seeds')
 colz=c.fetchall()
 
 for col in colz:
-    hotkey='0'
-    hotkey_stale='0'
-    #is_closure=True
-    while True:
-        #hotkey='0'
-        #hotkey_stale='0'
-        is_closure=True
-        #AS, CP=key.gen(int(hotkey))
-        req = request.Request(col[0].replace(hotkey_stale, hotkey), headers=headerz)
+    #req = request.Request(col[0].replace(hotkey_stale, hotkey), headers=headerz)
+    is_closure=True
+    for i in range(100):
+        req = request.Request(col[0].replace('0', str(time.time()-random.randint(0, 100000))), headers=headerz)
         rep = request.urlopen(req)
-        print(rep.getcode())
+        print(rep.getcode(), col[1], rep.getheader('set-cookie')), 
         jraw = gzip.decompress(rep.read())
         j = json.loads(jraw.decode('utf-8', 'ignore'))
         # get id list
         for newz in j['data']:
             try:
-                print(newz['item_id'], newz['tag'])
+                #print()
                 c.execute('insert into news(id, tag, title, source)values(?, ?, ?, ?)', (
                     newz['item_id'], newz['tag'], newz['title'], newz['source']
                 ))
                 db.commit()
                 is_closure=False
-                
+                print('success :',newz['item_id'], newz['tag'])
             except Exception as E:
-                print(E)
+                print('failed :', E)
+        
+        #time.sleep(1.2)
         if is_closure is True:
             break
-        hotkey_stale = hotkey
-        hotkey = str(random.randint(0, j['next']['max_behot_time']))
-        time.sleep(1.2)
